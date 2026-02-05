76cb6546125d8c5c411c06d6c54bc8ca1806a221
weyke <weyke01@gmail.com>
2026-02-05 11:19:46 -0300

    alteração de horario

--- script.js
+++ script.js
@@ -307,8 +307,8 @@ function verificarHorario() {
 
   // Atendimento das 18h às 22h
   const horarioPermitido =
-    (hora > 11 && hora < 22) ||
-    (hora === 11 && minuto >= 0) ||
+    (hora > 17 && hora < 22) ||
+    (hora === 17 && minuto >= 0) ||
     (hora === 22 && minuto <= 0);
 
   const lojaAberta = diaPermitido && horarioPermitido;
